8971ad32a86205204a2103af66c76a99beede36a
55yong <dhdydwns988@gmail.com>
2024-09-24 14:46:46 +0900

    올려올려올림판

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,7 +44,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/55yong/55yong.github.io/tree/main/packages/create-docusaurus/templates/shared/",
         },
         blog: {
           showReadingTime: true,
@@ -55,7 +55,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/55yong/55yong.github.io/tree/main/packages/create-docusaurus/templates/shared/",
           // Useful options to enforce blogging best practices
           onInlineTags: "warn",
           onInlineAuthors: "warn",
@@ -84,18 +84,12 @@ const config = {
         },
       ],
       navbar: {
-        title: "My Site",
+        title: "55yong blog",
         logo: {
           alt: "My Site Logo",
-          src: "img/logo.svg",
+          src: "img/logo.png",
         },
         items: [
-          // {
-          //   type: "docSidebar",
-          //   sidebarId: "tutorialSidebar",
-          //   position: "left",
-          //   label: "Tutorial",
-          // },
           {
             type: "docSidebar",
             sidebarId: "portfolio",
@@ -109,7 +103,7 @@ const config = {
             label: "🦁 멋사대학 12기 스터디",
           },
 
-          { to: "/blog", label: "Blog", position: "left" },
+          { to: "/blog", label: "✏️ TIL", position: "left" },
           {
             href: "https://github.com/55yong",
             label: "GitHub",
@@ -119,47 +113,47 @@ const config = {
       },
       footer: {
         style: "dark",
-        links: [
-          {
-            title: "Docs",
-            // items: [
-            //   {
-            //     label: "Tutorial",
-            //     to: "/docs/intro",
-            //   },
-            // ],
-          },
-          {
-            title: "Community",
-            // items: [
-            //   {
-            //     label: "Stack Overflow",
-            //     href: "https://stackoverflow.com/questions/tagged/docusaurus",
-            //   },
-            //   {
-            //     label: "Discord",
-            //     href: "https://discordapp.com/invite/docusaurus",
-            //   },
-            //   {
-            //     label: "Twitter",
-            //     href: "https://twitter.com/docusaurus",
-            //   },
-            // ],
-          },
-          {
-            title: "More",
-            items: [
-              {
-                label: "Blog",
-                to: "/blog",
-              },
-              {
-                label: "GitHub",
-                href: "https://github.com/55yong",
-              },
-            ],
-          },
-        ],
+        // links: [
+        //   {
+        //     title: "Docs",
+        //     // items: [
+        //     //   {
+        //     //     label: "Tutorial",
+        //     //     to: "/docs/intro",
+        //     //   },
+        //     // ],
+        //   },
+        //   {
+        //     title: "Community",
+        //     items: [
+        //       {
+        //         label: "LinkedIn",
+        //         href: "https://www.linkedin.com/in/55yong",
+        //       },
+        //       //   {
+        //       //     label: "Discord",
+        //       //     href: "https://discordapp.com/invite/docusaurus",
+        //       //   },
+        //       //   {
+        //       //     label: "Twitter",
+        //       //     href: "https://twitter.com/docusaurus",
+        //       //   },
+        //     ],
+        //   },
+        //   {
+        //     title: "More",
+        //     items: [
+        //       {
+        //         label: "TIL",
+        //         to: "/blog",
+        //       },
+        //       {
+        //         label: "GitHub",
+        //         href: "https://github.com/55yong",
+        //       },
+        //     ],
+        //   },
+        // ],
         copyright: `Copyright © ${new Date().getFullYear()} 55yong Inc. Built with Docusaurus.`,
       },
       prism: {
